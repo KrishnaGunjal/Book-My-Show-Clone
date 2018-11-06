6dfdfe8eddba1b0948fa1b0e495facaadb5e1b46
Krishna Gunjal <kgunjal51@gmail.com>
2018-11-06 19:08:58 +0530

    first commit

---
+++ BookMyShowClone/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  BookMyShowClone
+//
+//  Created by Krrish  on 21/10/18.
+//  Copyright © 2018 Krrish . All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BookMyShowClone/movies.swift
@@ -0,0 +1,9 @@
+//
+//  movies.swift
+//  BookMyShowClone
+//
+//  Created by Krrish  on 21/10/18.
+//  Copyright © 2018 Krrish . All rights reserved.
+//
+
+import Foundation
